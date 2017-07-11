64ed2babac39c770dc8e35704fbae63b63e9c479
machao123343 <790477302@qq.com>
2017-07-11 18:55:20 +0800

    something for pipe

--- C.go
+++ C.go
@@ -1,23 +1,90 @@
-package main
-
-import(
-    "errors"
-    "math"
-    "fmt"
-)
-
-func Sqrt(f float64) (float64 , error) {
-    if f < 0 {
-    return 0, errors.New("math: square root of negative number")
-    }
-    // implementation
-    return math.Sqrt(f) , errors.New(" ")
+stdout0 , err := cmd.StdoutPipe()
+if err := cmd).Start(); err != nil {
+    fmt.Printf("Error : the command No.0 can not be startup: %s\n" , err)
+    return
 }
-func main()  {
-    v , err := Sqrt(-1)
+
+stdout0 , err := cmd0.StdoutPipe()
+if err != nil {
+    fmt.Printf("Error : Can not obtain the stdout pipe for command No.0 : %s\n" , err)
+    return
+}
+
+if err := cmd.Start() ; err != nil {
+    fmt.Printf("Error : Can not obtain the stdout pipe for command No.0 : %s\n" , err)
+    return
+}
+
+output0 := make([]byte , 30)
+n , err = stdout0.Read(output0)
+if err != nil {
+    fmt.Printf("Error : Can not read data form the pipe : %s\n" , err)
+    return
+}
+fmt.Printf("%s\n" , output0[:n])
+
+var outputBuf0 bytes.Buffer
+
+for {
+    tempOutput := make([]byte , 5)
+    n , err := stdout0.Read(tempOutput)
     if err != nil {
-        fmt.Println(err)
+        if err == io.EOF {
+            break
+        } else {
+            fmt.Printf("Error: Can not read data from the pipe : %s\n" , err)
+            return
+        }
     }
-    fmt.Println(  v)
+    if n > 0 {
+        outputBuf0.Write(tempOutput[:n])
+    }
+}
+fmt.Printf("%s\n" , outputBuf0.String())
+cmd1 := exec.Command("ps" , "aux")
+cmd2 := exec.Command("grep" , "apipe")
+
+stdout1 , err := cmd1.StdoutPipe()
+if err != nil {
+    fmt.Printf("Error: Can not obtain the stdout pipe for command: %s\n" , err)
+    return
+}
+
+outputBuf1 := bufio.NewReader(stdout1)
+stdin2 , err := cmd2.StdinPipe()
+if err != nil {
+    fmt.Printf("Error: Can not obtain the stdin pipe for command: %s\n" , err)
+    return
+}
+outputBuf1.WriteTo(stdin2)
+var outputBuf2 bytes.buffer
+cmd2.Stdout = &outputBuf2
+
+//关闭输入管道
+if err := cmd2.Start(); err != nil {
+    fmt.Printf("Error: The command can not be startup: %s\n" , err)
+    return
+}
+err := stdin2.Close()
+if err != nil {
+    fmt.Printf("Error : Can not close the stdio pipe: %s\n" , err)
+}
+
+if err := cmd2.Wait(); err != nil {
+    fmt.Printf("Error : Can not wait for the command: %s\n" , err)
+    return
+}
+
+//     up
+reader , writer , err := os.Pipe()
+n , err := writer.Write(input)
+if err != nil {
+    fmt.Printf("Error: Can not write data to the named pipe: %s\n " , err)
+}
+fmt.Printf("Written %d byte(s) . [file-based pipe]\n" , n)
+out := make([]byte , 100)
+n , err := reader.Read(output)
+if err != nil {
+    fmt.Printf("Read %d byte(s) . [file-based pipe]\n" , n)
 }
 
